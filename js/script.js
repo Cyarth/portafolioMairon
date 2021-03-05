3ffc68a346d504ebc2e07c5b45f256b4905d9a78
cyarth <maironiv@icloud.com>
2021-03-05 17:53:30 -0300

    subir vista inventarioDuoc y proyectoDuoc

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ function scrollUp(){
 
     if (currentScroll > 0){
         window.requestAnimationFrame(scrollUp);
-        window.scrollTo (0, currentScroll - (currentScroll / 0));
+        window.scrollTo (0, currentScroll - (currentScroll / 2));
     }
 }
 
@@ -28,3 +28,31 @@ window.onscroll = function(){
     }
 
 }
+
+//darkswitch
+
+var scrollToTopBtn = document.querySelector(".scrollToTopBtn")
+var rootElement = document.documentElement
+
+function handleScroll() {
+  // Do something on scroll
+  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
+  if ((rootElement.scrollTop / scrollTotal ) > 0.80) {
+    // Show button
+    scrollToTopBtn.classList.add("showBtn")
+  } else {
+    // Hide button
+    scrollToTopBtn.classList.remove("showBtn")
+  }
+}
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+scrollToTopBtn.addEventListener("click", scrollToTop)
+document.addEventListener("scroll", handleScroll)
+  
